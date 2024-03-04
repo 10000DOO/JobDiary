645d8e996fc684373e7793c0e7e51f0ab7b3f4f8
10000DOO <gunjoun99@gmail.com>
2024-03-04 12:54:06 +0900

    Initial Commit

---
+++ JobDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobDiary
+//
+//  Created by 이건준 on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobDiary/JobDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobDiaryApp.swift
+//  JobDiary
+//
+//  Created by 이건준 on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct JobDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
